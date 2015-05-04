7375820a4726e45a02c245ac2d5e1eeef0d55f0d
Dane Thurber <dane.thurber@gmail.com>
2015-05-04 13:46:24 -0600

    add examples for floats and clearing

--- server/index.js
+++ server/index.js
@@ -25,6 +25,7 @@ server.route({
 
 var examples = ['box-model', 'box-sizing', 'document-flow', 'constrained-width']
 examples = examples.concat(['media-queries', 'mobile-first', 'include-media', 'column-drop'])
+examples = examples.concat(['floats', 'clearfix'])
 examples = examples.concat(['positioning', 'z-index'])
 examples = examples.concat(['gutter-pull'])
 
